3882e5d6a4e4879ad2394a743b479edc05f3bef7
Alfin G <119277850+alfindeva@users.noreply.github.com>
2023-04-09 18:56:36 +0700

    Membuat CV

---
+++ application/views/CV_Alfin.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curriculum Vitae</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <style>
+        body {
+            background-color: #f7f7f7;
+        }
+
+        h1, h2 {
+            font-family: 'Roboto', sans-serif;
+            color: #333333;
+            font-weight: bold;
+            margin-bottom: 20px;
+            text-align: center;
+            text-transform: uppercase;
+        }
+
+        .container {
+            margin-top: 50px;
+            background-color: #ffffff;
+            padding: 30px;
+            box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+        }
+
+        ul {
+            margin-left: 20px;
+            margin-bottom: 0;
+        }
+
+        ul li {
+            list-style: none;
+            margin-bottom: 5px;
+        }
+
+        ul li:before {
+            content: '\2022';
+            margin-right: 5px;
+            color: #333333;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <img src="Polish_20221013_142855825 (1).jpg" alt="Curriculum Vitae" class="img-fluid mx-auto d-block" width="100" height="100">
+        <br>
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Curriculum Vitae</h1>
+            </div>
+        </div>
+        <hr >
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Biodata</h2>
+                <ul>
+                    <li>Nama : Alfin Gerliandeva</li>
+                    <li>Email : alvingerliandeva20@if.unjani.ac.id</li>
+                    <li>No HP : 087816742401</li>
+                </ul>
+            </div>
+            <div class="col-md-6">
+                <h2>Edukasi</h2>
+                <ul>
+                    <li>SDN Sukamaju 1 Cimahi (2007 - 2013)</li>
+                    <li>SMP Negeri 8 Cimahi (2013 - 2016)</li>
+                    <li>SMK TI Garuda Nusantara Cimahi (2016 - 2019)</li>
+                    <li>Universitas Jenderal Achmad Yani (2020 - 2024) Aamiin :)</li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Kemampuan</h2>
+                <ul>
+                    <li>Bahasa Pemrograman : HTML, PHP dan Java</li>
+                    <li>Basis Data : MySQL, MariaDB</li>
+                </ul>
+            </div>
+            <div class="col-md-6">
+                <h2>Pengalaman</h2>
+                <ul>
+                    <li>Magang / Praktik Kerja Lapangan, UNJANI (2018)</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6/"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.js" integrity="sha384-rAsz3nGQ2Za1MBhVY2vJ75C7VjKX9KNh7gzPzrZwGr7V1zKj8+blWmNFj4bkTXO/" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-08B/xtj+1FbJ0TV/D6ZPTxK6fsmg6Uv3qobAn2QyX49BjKk1e91CnZp/f19xR+8W" crossorigin="anonymous"></script>
+</body>
+</html>
