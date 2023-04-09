148797fb4206d8a6e37252a7eb01d3c96224b21d
NFadil <75855353+NFadil@users.noreply.github.com>
2023-04-09 21:13:09 +0700

    tambah Navigasi

--- application/views/Cv_Nurul.php
+++ application/views/Cv_Nurul.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html>
 <head>
@@ -58,64 +57,3 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
-=======
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Curriculum Vitae</title>
-  <!-- Tambahkan link CSS untuk Bootstrap -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <style>
-    /* Menambahkan latar belakang berwarna bergerak */
-    body {
-      background: linear-gradient(to right, #ff5f6d, #ffc371, #fffc00);
-      background-size: 400% 400%;
-      animation: gradient 10s ease infinite;
-    }
-    
-    /* Animasi latar belakang berwarna bergerak */
-    @keyframes gradient {
-      0% {
-        background-position: 0% 50%;
-      }
-      50% {
-        background-position: 100% 50%;
-      }
-      100% {
-        background-position: 0% 50%;
-      }
-    }
-    .white-box {
-            background-color: white;
-            padding: 10px;
-            border-radius: 5px;
-        }
-  </style>
-</head>
-<body>
-  <div class="container mt-5 white-box " >
-  
-    <h1 class="display-1 ">Curriculum Vitae</h1>
-    <div class="row mt-3  ">
-      <div class="col-md-6">
-        <h2 class="display-4 ">Personal Information</h2>
-        <ul>
-        <p><strong>Name:</strong> Nurul Fadhilah Anwar</p>
-        <p><strong>Email:</strong> Nurulfadhilaha20@if.unjani.ac.id</p>
-        <p><strong>Phone:</strong> +6288312435692</p>
-        <p><strong>Address:</strong> Jl.Kolmas no.17</p>
-        </ul>
-      </div>
-      <div class="col-md-6 ">
-        <h2 class="display-4">Education</h2>
-        <ul> 
-            <h5>Prodi Informatika</h5>
-            <p>Universitas Jendral Achmad Yani</p>
-            <p>Graduation Year: 2024 Amin</p>
-        </ul>
-      </div>
-    </div>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
-</html>
->>>>>>> 909b5dc0a52dd56da597f46c652fdecfee76f6cb
