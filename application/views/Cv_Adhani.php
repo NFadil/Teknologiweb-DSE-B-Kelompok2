bfa1d9bc8d9f69f5b722cee806e06e71eaf38a95
AdhaniMulianti <75653809+AdhaniMulianti@users.noreply.github.com>
2023-04-09 20:52:44 +0700

    Add files via upload

---
+++ application/views/Cv_Adhani.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Curriculum Vitae</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body>
+
+<div class="resume-box">
+	
+	<div class="left-section">
+		<div class="profile">
+		<img src="image/hani.png" class="profile-img">
+		<div class="blue-box"></div>
+		</div>
+		<h2 class="name">Adhani<br><span>Mulianti</span></h2>
+		<p class="n-p">04 Maret 2001</p>
+
+
+		<div class="info">
+			<p class="heading">Info</p>
+			<p class="p1"><span class="span1"><img src="image/location.png"></span>Alamat<span> <br>Purwakarta</span></p>
+			<p class="p1"><span class="span1"><img src="image/call.png"></span>Phone<span> <br>081572185224</span></p>
+			<p class="p1"><span class="span1"><img src="image/mail.png"></span>Email<span> <br>adhanimulianti20@if.unjani.ac.id</span></p>
+		</div>
+
+		<div class="info">
+			<p class="heading">Media Sosial</p>
+			<p class="p1"><span class="span1"><img src="image/ig.png"></span>Instagram<span> <br>@adhanimulianti</span></p>
+			<p class="p1"><span class="span1"><img src="image/twitter.png"></span>Twitter<span> <br>bajiguree</span></p>
+		</div>
+
+	</div>
+
+	<div class="right-section">
+		<div class="right-heading">
+			<img src="image/about me.png">
+			<p class="p2">About Me</p>
+		</div>
+		<p class="p3"></p>
+
+		<div class="clearfix"></div>
+		<br><br>
+		<div class="right-heading">
+			<img src="image/toga.png">
+			<p class="p2">Education</p>
+		</div>
+		<div class="clearfix"></div>
+		<div class="lr-box">
+		<div class="left">
+			<p class="p4">2007 - 2013</p>
+			<p class="p5">Purwakarta</p>
+		</div>
+
+		<div class="right">
+			<p class="p4">SDN 2 Bunder</p>
+		</div>
+		<div class="clearfix"></div>
+		</div>
+
+		<div class="lr-box">
+		<div class="left">
+			<p class="p4">2013 - 2016</p>
+			<p class="p5">Subang</p>
+		</div>
+
+		<div class="right">
+			<p class="p4">Mts PP Darussalam</p>
+		</div>
+		<div class="clearfix"></div>
+		</div>
+
+		<div class="lr-box">
+		<div class="left">
+			<p class="p4">2016 - 2019</p>
+			<p class="p5">Purwakarta</p>
+		</div>
+
+		<div class="right">
+			<p class="p4">SMAN 1 Purwakarta</p>
+		</div>
+		<div class="clearfix"></div>
+		</div>
+
+		<div class="lr-box">
+			<div class="left">
+				<p class="p4">2020 - 2024</p>
+				<p class="p5">Cimahi</p>
+			</div>
+	
+			<div class="right">
+				<p class="p4">Universitas Jenderal Achmad Yani</p>
+			</div>
+			<div class="clearfix"></div>
+			</div>
+
+		
+		<br>
+		<div class="right-heading">
+			<img src="image/skill.png">
+			<p class="p2">Skill</p>
+		</div>
+		<div class="clearfix"></div>
+		<div class="s-box">
+			<p class="p6">Microsoft Word</p>
+			<div id="progress"></div>
+			<p class="p6">Microsoft Excel</p>
+			<div id="progress1"></div>
+			
+		</div>
+		<div class="s-box">
+			<p class="p6">HTML</p>
+			<div id="progress2"></div>
+			<p class="p6">CSS</p>
+			<div id="progress3"></div>
+		</div>
+
+
+		<div class="clearfix"></div>
+		<br><br>
+		<div class="right-heading">
+			<img src="image/hobi.png">
+			<p class="p2">Hobby</p>
+		</div>
+		<div class="clearfix"></div>
+		<img src="image/bicycle.png" class="h-img">
+		<img src="image/games.png" class="h-img">
+		<img src="image/book.png" class="h-img">
+	</div>
+
+		<div class="clearfix"></div>
+</div>
+</body>
+</html>
