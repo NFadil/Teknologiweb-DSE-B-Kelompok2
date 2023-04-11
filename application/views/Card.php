b14a235d12e1536597f2abe5c5fc371d92561d23
Alfin Gerliandeva <alvingerliandeva20@if.unjani.ac.id>
2023-04-11 23:34:41 +0700

    Update Card.php

--- application/views/Card.php
+++ application/views/Card.php
@@ -9,32 +9,27 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     </head>
     <body>
-    <div class="card-deck">
-  <div class="card">
-    <img src= "http://localhost/CI_Tekweb_kel2/Teknologiweb-DSE-B-Kelompok2/assets/image/profil.jpg" class="card-img-top"width="100" height="600" alt="...">
-    <div class="card-body">
-      <a class="card-title" href="http://localhost/CI_Tekweb_kel2/Teknologiweb-DSE-B-Kelompok2/index.php/Home/CV1">Nurul Fadhilah Anwar </a>
-      
+  <div class="card-deck">
+    <div class="card">
+      <img src= "http://localhost/CI_Tekweb_kel2/Teknologiweb-DSE-B-Kelompok2/assets/image/profil.jpg" class="card-img-top" width="100" height="600" alt="...">
+      <div class="card-body">
+        <a class="card-title" href="http://localhost/CI_Tekweb_kel2/Teknologiweb-DSE-B-Kelompok2/index.php/Home/CV1">Nurul Fadhilah Anwar</a>
+      </div>
+    </div>
+
+    <div class="card">
+      <img src= "http://localhost/CI_Tekweb_kel2/Teknologiweb-DSE-B-Kelompok2/assets/image/Polish_20221013_142855825 (1).jpg" class="card-img-top" width="100" height="600" alt="...">
+      <div class="card-body">
+        <a class="card-title" href="http://localhost/CI_Tekweb_kel2/Teknologiweb-DSE-B-Kelompok2/index.php/Home/CV2">Alfin Gerliandeva</a>
+      </div>
+    </div>
+
+    <div class="card">
+      <img src= "http://localhost/CI_Tekweb_kel2/Teknologiweb-DSE-B-Kelompok2/assets/image/hani.png" class="card-img-top" width="100" height="600" alt="...">
+      <div class="card-body">
+        <a class="card-title" href="http://localhost/CI_Tekweb_kel2/Teknologiweb-DSE-B-Kelompok2/index.php/Home/CV3">Adhani Mulianti</a>
+      </div>
     </div>
   </div>
-  <div class="card">
-    <img src="http://localhost/CI_Tekweb_kel2/Teknologiweb-DSE-B-Kelompok2/assets/image/Polish_20221013_142855825 (1).jpg" class="card-img-top"width="100" height="600" alt="...">
-    <div class="card-body">
-      <a  class="card-title" href="http://localhost/CI_Tekweb_kel2/Teknologiweb-DSE-B-Kelompok2/index.php/Home/CV2">Alfin Gerlian</h5>
-      
-    </div>
-  </div>
-  <div class="card">
-    <img src="http://localhost/CI_Tekweb_kel2/Teknologiweb-DSE-B-Kelompok2/assets/image/hani.png" class="card-img-top" width="100" height="600"alt="...">
-    <div class="card-body">
-      <h5 class="card-title" href="http://localhost/CI_Tekweb_kel2/Teknologiweb-DSE-B-Kelompok2/index.php/Home/CV3" >Adhani Mulianti</h5>
-      
-      
-    </div>
-  </div>
-</div>
-      
-        
-        
-    </body>
+</body>
 </html>
